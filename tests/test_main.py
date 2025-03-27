5812bfbb5d260cc95a88d85c144cf7135a42bf2d
Fabio0703 <fabio.bostanci@btc-ag.com>
2025-03-28 00:17:53 +0100

    commit

---
+++ tests/test_main.py
@@ -0,0 +1,13 @@
+from app.main import greet,is_even,add
+
+def test_greeting():
+    assert greet("Oldenburg") == "Hallo, Oldenburg"
+
+def test_is_even_true():
+    assert is_even(8) is True
+
+def test_is_even_false():
+    assert is_even(9) is False
+
+def test_addition():
+    assert add(2,3) == 5
