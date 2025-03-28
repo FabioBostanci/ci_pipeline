4f9030ccceae5d1ec0b2c2cbcf9a393c9bbdba6e
Fabio0703 <fabio.bostanci@btc-ag.com>
2025-03-28 09:11:27 +0100

    Formatierter Code mit black

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 def greet(name=None):
     return f"Hallo, {name}"
 
+
 def is_even(number):
     return number % 2 == 0
 
-def add(a,b):
-    return a+b
 
-if __name__ == '__main__':
+def add(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
     print(greet())
-    print("10 ist gerade:",is_even(10))
-    print("2 + 3 =",add(2,3))
+    print("10 ist gerade:", is_even(10))
+    print("2 + 3 =", add(2, 3))
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,17 @@
-from app.main import greet,is_even,add
+from app.main import greet, is_even, add
+
 
 def test_greeting():
     assert greet("Oldenburg") == "Hallo, Oldenburg"
 
+
 def test_is_even_true():
     assert is_even(8) is True
+
 
 def test_is_even_false():
     assert is_even(9) is False
 
+
 def test_addition():
-    assert add(2,3) == 5
+    assert add(2, 3) == 5
